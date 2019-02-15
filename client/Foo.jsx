cae3da395507525bfd89a484f5c9904328d29c4e
sharmoo <amberpsharma@gmail.com>
2019-02-14 23:48:49 -0800

    Testharness (#2)
    * Adds Jest as dev depenendency
    
    * Adds react and enzyme dependencies
    
    * Added webpack as a dependency, created jest config file, added basic jest test, added placeholder to test React component, and set up jest to use enzyme
    
    * Adds webpack.config file, adds build-dev npm script, added index.js as entry point
    
    * Adds babel dependencies
    
    * Added Foo component as a test, added enzyme test suite for Foo component, added webpack and babel as dependencies. Added testing to circle ci
    
    * Removes full DOM test temporarily
    
    * Add index.html, boostrap react application with mount of Foo component, added hot loader as dependency

---
+++ client/Foo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Foo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="foo">Hello Foo</div>
+    )
+  }
+}
+
+export default Foo;
---
+++ client/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Foo from './Foo';
+
+ReactDOM.render(<Foo />, document.getElementById('app'));
+
+module.hot.accept();
