0d5aab5ad9b40d9374a690c5fe633ba5ff7144b8
Amber Sharma <shrama.amber.p@gmail.com>
2019-02-19 15:00:40 -0800

    Fixed express static file server

--- server/index.mjs
+++ server/index.mjs
@@ -7,9 +7,9 @@ const connection = mysql.createConnection({
   database: 'zoundloud',
 });
 
-var app = express();
+const app = express();
 app.use(express.json());
-
+app.use(express.static('dist'));
 // static server here for dist files in production...
 
 // endpoints
@@ -17,37 +17,37 @@ app.use(express.json());
 // api/users
 app.get('/api/users', (req, res) => {
   // do a SELECT * from the database
-  connection.query(`select * from users`, (err, dbres) => {
+  connection.query('select * from users', (err, dbres) => {
     if (err) {
-      res.json(err)
+      res.json(err);
     }
     res.json(dbres);
-  })
+  });
 });
 
 // api/songs
 app.get('/api/songs', (req, res) => {
   // do a SELECT * from the database
-  connection.query(`select * from songs`, (err, dbres) => {
+  connection.query('select * from songs', (err, dbres) => {
     if (err) {
-      res.json(err)
+      res.json(err);
     }
     res.json(dbres);
-  })
+  });
 });
 
 // api/playlists
 app.get('/api/playlists', (req, res) => {
   // do a SELECT * from the database
-  connection.query(`select * from playlists`, (err, dbres) => {
+  connection.query('select * from playlists', (err, dbres) => {
     if (err) {
-      res.json(err)
+      res.json(err);
     }
     res.json(dbres);
-  })
+  });
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
-})
+});
