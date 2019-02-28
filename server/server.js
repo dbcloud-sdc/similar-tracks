dae495f3644cb5c2d3fd13473f70db7f187d8789
Jonathan Mitzman <Jonathan.mitzman@gmail.com>
2019-02-28 15:04:43 -0800

    Created basic server skeleton (see details on next line) Imported configuration settings Imported database functions and connections Configured port based on CONFIG Routed all api types: client(index+bundle), 'similar songs', all CRUD functions (create, read, update, delete) Allowed for conditional CORS policy Server start code (IFFE)

--- server/server.js
+++ server/server.js
@@ -1,40 +1,99 @@
-import CONFIG from './config.js';
-// TODO: if this file doesnt exist, it will cause an error.
+//NOTE: this is a skeleton/stub file
+//Many of the status codes and specific functionality has not been completed.
 
+import CONFIG from '../config.js';
+import DB from './db/db.js';
 const express = require('express');
 const server = express();
 const fs = require('fs');
 const path = require('path');
+const port = CONFIG.SERVER.PORT;
 
+//********************** SERVER ROUTES **********************
 
-const port = CONFIG.SERVER.PORT || process.env.PORT || 3000;
+const route = {
+  serveClient: () => {
+    server.use('/song/:songID', express.static(path.join(__dirname, '../dist/')));
+  },
+  serveSongs: () => {
+    server.get('/api/song/:id/relatedtracks', (req, res) => {
+      let id = req.params.id;
+      DB.getSongs()
+        .then(songs => {
+          //TODO: send songs
+          //TODO: status codes (200)
+        })
+        .catch(err => {
+          //TODO: error codes
+          console.log(err);
+        })
+    })
+  },
+  create: () => {
+    server.post('/api/song/:id', (req, res) => {
+      let id = req.params.id;
+      let record = null; //TODO: define document
+      DB.create(id, record)
+        .then(() => {}) //TODO: success code (201)
+        .catch((err) => {});//TODO: failure status codes
+    });
+  },
+  read: () => {
+    server.get('/api/song/:id', (req, res) => {
+      let id = req.params.id;
+      DB.read(id)
+        .then(() => {})  //TODO: success code (200), send record
+        .catch(() => {});//TODO: failure status codes
+    });
+  },
+  update: () => {
+    server.put('/api/song/:id', (req, res) => {
+      let id = req.params.id;
+      let newRecord = null; //TODO: define document
+      DB.update(id, newRecord)
+        .then(() => {})//TODO: success code (200? 202?), send record
+        .catch(() => {});//TODO: failure status codes
+    });
+  },
+  delete: () => {
+    server.delete('/api/song/:id', (req, res) => {
+      let id = req.params.id;
+      DB.delete(id)
+        .then(() => {}) //TODO: success code?
+        .catch(() => {}) //TODO: error code/response
+    })
+  }
+};
 
+//************** SERVER-SIDE PROCESSING FUNCTIONS **************
 
-/* API:
- *
- * GET: /api/song/:id/users
- * Expects JSON result in array of 9
- * -> Needs to take the song ID, and then query the database for users
- * that have liked that song.  Return 8 via some methodology.
- *
- *
- * GET: /api/song/:id/relatedtracks
- * Expects JSON result in array of 3
- * Need to have a way to query the username from the SongID,
- * and return an array of 3 songs from the same user.
- * (WHAT IF THE USER DOESNT HAVE THREE SONGS...?)
- *
- *
- * GET: /api/song/:id/playlists
- * Expects: JSON result in an array of 3
- *
- *
- */
+//TODO: not needed for now - placeholder.
+
+//********************** SECURITY CONFIG **********************
+
+const cors = () => {
+  if (CONFIG.CORS.ENABLED) {
+    server.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', `${CONFIG.CORS.ORIGIN}`);
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      res.header('Access-Control-Allow-Methods', `${CONFIG.CORS.METHODS}`);
+      console.log(`CORS has been enabled for the server`);
+      if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+      }
+      next();
+    });
+  } else {
+      console.log(`Warning: server is not allowing CORS`);
+  }
+}
 
 //********************** START SERVER **********************
 ((initialize) => {
   //functions here
-
+  cors();
+  route.serveClient();
+  route.serveSongs();
   server.listen(port, () => {
     console.log(`listening on port ${port}`);
   });
