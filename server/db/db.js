7bdc706c5c4f367b760caaec2969046578888210
Jonathan Mitzman <Jonathan.mitzman@gmail.com>
2019-02-28 15:06:33 -0800

    Created database module

---
+++ server/db/db.js
@@ -0,0 +1,26 @@
+//TODO: depedencies
+//TODO: signal flow for SQL vs No-SQL (TBD - after seed)
+
+const DB = {
+  getSongs: (id) => {
+    //placeholder
+    return new Promise();
+  },
+  create: (id, record) => {
+    //placeholder
+    return new Promise();
+  },
+  read: (id) => {
+    //placeholder
+    return new Promise();
+  },
+  update: (id, record) => {
+    //placeholder
+    return new Promise();
+  },
+  delete: (id) => {
+    return new Promise();
+  }
+};
+
+export default DB;
