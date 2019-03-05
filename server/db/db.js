6b4c13789300bbc44534c79bfe51e128a9a83ed3
Jonathan Mitzman <Jonathan.mitzman@gmail.com>
2019-03-05 14:28:05 -0800

    Reduced footprint of db.js (DRY)

--- server/db/db.js
+++ server/db/db.js
@@ -11,25 +11,13 @@ const client = new pg.Client({
 
 client.connect(); //TODO: determine if the connection should be open/closed or remain open
 
-const query = (term) => {
-  return new Promise((resolve, reject) => {
-    client.query(term, (err, res) => {
-      if (err) reject(err);
-      else     resolve(res);
-    });
-  });
-}
-
 module.exports = {
-  getRelatedSongs: (id) => {
-    let term = `SELECT * FROM songs WHERE
-               id = (SELECT songa FROM related WHERE id=${id})
-            OR id = (SELECT songb FROM related WHERE id=${id})
-            OR id = (SELECT songc FROM related WHERE id=${id});`
-    return query(term);
-  },
-//***** For updating the related table via CRUD *****/
   process: (term) => {
-    return query(term);
+    return new Promise((resolve, reject) => {
+      client.query(term, (err, res) => {
+        if (err) reject(err);
+        else     resolve(res);
+      });
+    });
   },
 };
