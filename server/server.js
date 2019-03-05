1b6d43a64c79bc9db4872437335dec9fde5ac6bf
Jonathan Mitzman <Jonathan.mitzman@gmail.com>
2019-03-05 12:30:02 -0800

    Completed UPDATE endpoint

--- server/server.js
+++ server/server.js
@@ -11,45 +11,60 @@ const route = {
   serveClient: () => {
     server.use('/song/:songID', express.static(path.join(__dirname, '../dist/')));
   },
-  serveSongs: () => {
+
+  serveSongs: () => { //This is the microservice endpoint.
     server.get('/api/song/:id/relatedtracks', (req, res) => {
       let id = req.params.id;
-      database.getSongs(id)
+      database.getRelatedSongs(id)
         .then(songs => {
           res.send(convert.format(songs.rows)).status(200).end();
-        })
-        .catch(err => {
+      }).catch(err => {
+          console.log('Failure retrieving related tracks from the database:');
           console.log(err);
           res.status(500).end();
-        })
-    })
+      });
+    });
   },
-  createSong: (record) => {
-    server.post('/api/song/:id', (req, res) => {
+
+  createRelation: () => {
+    server.post('/api/related/:id', (req, res) => {
       let id = req.params.id;
+      console.log(req.body);
+      let relationship = null; //TODO: process the req.body
       let term = ``; //TODO: define SQL command
       database.create(id, term)
         .then(() => {}) //TODO: success code (201)
         .catch((err) => {});//TODO: failure status codes
     });
   },
-  readSong: () => {
-    server.get('/api/song/:id', (req, res) => {
+
+  readRelation: () => {
+    server.get('/api/related/:id', (req, res) => {
       let id = req.params.id;
+      let term = ``; //TODO: define SQL command
       database.read(id)
         .then(() => {})  //TODO: success code (200), send record
         .catch(() => {});//TODO: failure status codes
     });
   },
-  updateSong: () => {
-    server.put('/api/song/:id', (req, res) => {
+
+  updateRelation: () => {
+    server.put('/api/related/:id', (req, res) => {
       let id = req.params.id;
-      let newRecord = null; //TODO: define document
-      database.update(id, newRecord)
-        .then(() => {})//TODO: success code (200? 202?), send record
-        .catch(() => {});//TODO: failure status codes
+      let relationship = req.body;
+      if(validate.relationUpdate(relationship, id)) {
+        let term = convert.toUpdateQuery(relationship);
+        console.log(term);
+        database.update(id, newRecord)
+          .then(() => {})//TODO: success code (200? 202?), send record
+          .catch(() => {});//TODO: failure status codes
+      } else {
+        console.log('Rejecting invalid update request');
+        res.status(422).end();
+      }
     });
   },
+
   deleteSong: () => {
     server.delete('/api/song/:id', (req, res) => {
       let id = req.params.id;
@@ -73,6 +88,56 @@ const convert = {
       };
     });
     return data;
+  },
+  toUpdateQuery: (data) => {
+    //need to determine what parameters exist on data,
+    //it can be songa, songb, and/or songc
+    //then generate a postgreSQL query and return it as a string
+    let set = '';
+    let id = null;
+    for (let key in data) {
+      if(key === 'id') {
+        id = data[key];
+      } else {
+        set += `${key} = ${data[key]},`;
+      }
+    }
+    set = set.substring(0,set.length-2); //remove trailing comma
+    return `UPDATE related SET ${set} WHERE id = ${id};`;
+  }
+}
+
+const validate = {
+  relationUpdate: (relation, id) => {
+    //Perform very basic validation.
+    //TODO: check input lengths
+    const validEntries = {
+      'id': 'number',
+      'songa': 'number',
+      'songb': 'number',
+      'songc': 'number'
+    };
+
+    if (Number(relation.id) !== Number(id)) {
+      console.log(`Inputed ID is ${relation.id} does not match endpoint (${id})`);
+      return false;
+    }
+    if (typeof relation !== 'object') {
+      console.log('Update request is not an object');
+      return false;
+    }
+    for (let key in relation) {
+      if(!validEntries[key]) {
+        console.log(`${key} does not exist in validEntries`);
+        return false;
+      } else {
+        if (typeof relation[key] !== validEntries[key]) {
+          console.log(`Typeof ${key} is ${typeof relation[key]} instead of ${validEntries[key]}`);
+          return false;
+        }
+      }
+    }
+    return true;
   }
 }
 
@@ -97,18 +162,21 @@ const cors = () => {
 
 //********************** START SERVER **********************
 ((initialize) => {
-  //SECURITY:
+  //SECURITY & CONFIGURATION:
   cors();
+  server.use(express.json()); //TODO: Currently, the server will crash if it doesn't receive JSON...
 
   //CLIENT:
   route.serveClient();
+
+  //Primary API call from client:
   route.serveSongs();
 
-  //CRUD: TODO: enable these
-  route.readSong();
-  route.updateSong();
+  //CRUD operations on primary table (related):
+  route.createRelation();
+  route.readRelation();
+  route.updateRelation();
   route.deleteSong();
-  route.createSong();
 
   //EXPOSE:
   server.listen(port, () => {
