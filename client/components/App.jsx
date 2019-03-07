81442e094dfbbd9edf22cfc6d5bd35b583a62952
Jonathan Mitzman <Jonathan.mitzman@gmail.com>
2019-03-06 18:58:09 -0800

    Updated client to use relative GET paths for proxy integration

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,7 +5,6 @@ import PlaylistList from './PlaylistList';
 import BubbleList from "./BubbleList";
 import TracksHover from "./TracksHover";
 import HCDATA from '../hcdata.js';
-import CONFIG from '../../config.js';
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -29,8 +28,8 @@ class App extends React.Component {
     //note, this endpath is not correct, so I fixed it.
     //the user should navigate to /song/1, not /song/1/, which implies directory access, instead of file access
     const id = window.location.pathname;
-    return fetch(`http://${CONFIG.SERVER.URL}:${CONFIG.SERVER.PORT}/api${id}relatedtracks`)
-      .then(response => {
+    return fetch(`/api${id}relatedtracks`)
+    .then(response => {
         return response.json()
       }
       )
