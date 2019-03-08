882b119e953b302f9202fc5a3e83d8693e0b8d20
Jonathan Mitzman <Jonathan.mitzman@gmail.com>
2019-03-08 14:22:44 -0800

    Implemented pooling

--- server/db/db.js
+++ server/db/db.js
@@ -9,12 +9,26 @@ const client = new pg.Client({
   port: CONFIG.POSTGRES.PORT,
 });
 
-client.connect(); //TODO: determine if the connection should be open/closed or remain open
+const pool = new pg.Pool({
+  user: CONFIG.POSTGRES.USER,
+  host: CONFIG.POSTGRES.URL,
+  database: CONFIG.POSTGRES.DBNAME,
+  password: CONFIG.POSTGRES.PASS,
+  port: CONFIG.POSTGRES.PORT,
+
+  // idleTimeoutMillis: 20000,
+  // connectionTimeoutMillis: 3000,
+  max: 50 //default
+});
+
+pool.connect();
+// client.connect(); //TODO: determine if the connection should be open/closed or remain open
 
 module.exports = {
   process: (term) => {
     return new Promise((resolve, reject) => {
-      client.query(term, (err, res) => {
+      //IMPORTANT: no releaseCallback is required because we are only performing a single query.
+      pool.query(term, (err, res) => {
         if (err) reject(err);
         else     resolve(res);
       });
